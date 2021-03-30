5182866d1a7e505845f4dbf6228ea0d4bf382427
ecuellarfernandez <ecuellarfernandez@gmail.com>
2021-03-30 11:30:34 +0200

    appComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import youtube from './apis/youtube';
+import { Container } from "@material-ui/core";
+
 function App() {
+
+  // const [videos, setVideos] = useState([]);
+  // const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const handleSubmit = async (termReceivedFromSearchBar)=>{
+    try{
+      const res = await youtube.get('/search', {
+        params:{
+          q: termReceivedFromSearchBar
+        }
+      })
+
+      console.log(res.data.items);
+      
+    }catch(e){
+      console.error(e)
+    }
+  }
+
+  // const handleVideoSelect = ()=>{
+
+  // }
+
   return (
-    <h1>hola</h1>
+    <Container>
+      <SearchBar submitHandler={handleSubmit}/>
+      {/* <VideoDetail /> */}
+      {/* <VideoList /> */}
+    </Container>
   );
 }
 
