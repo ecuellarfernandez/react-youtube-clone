7fa22f80a85b34a36b5041806101f20c7dabc66f
ecuellarfernandez <ecuellarfernandez@gmail.com>
2021-04-03 17:35:20 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 ReactDOM.render(
-  <Fragment>
-    <CssBaseline />
-    <App />
-  </Fragment>,
+  <App />,
   document.getElementById('root')
 );
