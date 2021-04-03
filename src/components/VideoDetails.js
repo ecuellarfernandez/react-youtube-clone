f4c5e8b7ed0c8776604cb860a7d263d1c4ac9189
ecuellarfernandez <ecuellarfernandez@gmail.com>
2021-04-03 17:34:37 +0200

    videoDetailComponent

---
+++ src/components/VideoDetails.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const VideoDetails = (props) => {
+    const {currentVideo} = props;
+
+    if(!currentVideo){
+        return <div>Start a search by term in the top bar</div>
+    }
+
+    const urlForIframe = `https://www.youtube.com/embed/${currentVideo.id.videoId}?autoplay=1`
+
+    return (
+        <div className='current-video'>
+            <iframe
+            className='current-video__video'
+            src={urlForIframe}
+            title='YouTube video player'
+            allowFullScreen
+            ></iframe>
+
+            <div className='current-video__description'>
+                <h1>{currentVideo.snippet.title}</h1>
+                <p>{currentVideo.snippet.description}</p>
+            </div>
+        </div>
+    )
+}
+
+export default VideoDetails
