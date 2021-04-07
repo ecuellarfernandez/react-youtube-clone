95482497919e6f0a872c39e90eeb0b0f4e1b6ba1
ecuellarfernandez <ecuellarfernandez@gmail.com>
2021-04-07 08:45:13 +0200

    Router, estado selectedVideo eliminado pasa a VideoDetails

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
-import youtube from './apis/youtube';
+import {youtube} from './apis/youtube';
 import VideoList from "./components/VideoList";
-import VideoDetails from "./components/VideoDetails";
 import './static/css/main.scss';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import VideoDetails from './components/VideoDetails';
 
 function App() {
 
   const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
 
   const handleSubmit = async (termReceivedFromSearchBar)=>{
     try{
       const res = await youtube.get('/search', {
         params:{
           q: termReceivedFromSearchBar,
+          maxResults:'5',
           type: 'video'
         }
       })
       setVideos(res.data.items);
-      setSelectedVideo(res.data.items[0]);
     }catch(e){
       console.error(e)
     }
   }
 
-  const handleVideoSelect = (video)=>{
-    console.log(video)
-    setSelectedVideo(video)
-  }
-
   return (
-    <div id='wrapper'>
-        <SearchBar submitHandler={handleSubmit}/>
-        <VideoDetails currentVideo={selectedVideo}/>
-        <VideoList list={videos} handleVideoSelect={handleVideoSelect}/>
-    </div>
+    <Router>
+
+      <Link to="/">Inicio</Link>
+
+      <div id='wrapper'>
+    <Switch>
+
+        <Route path='/video/:id'>
+          <SearchBar submitHandler={handleSubmit}/>
+          <VideoDetails />
+        </Route>
+
+        <Route path='/' exact>
+          <SearchBar submitHandler={handleSubmit}/>
+          <VideoList list={videos} />
+        </Route>
+
+        <Route path="*">
+            <h1>No match</h1>
+        </Route>
+
+    </Switch>
+
+      </div>
+    </Router>
   );
 }
 
