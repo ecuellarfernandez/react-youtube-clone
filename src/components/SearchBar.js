298a80adca9d02dde870213dae039de1860f5001
ecuellarfernandez <ecuellarfernandez@gmail.com>
2021-04-07 08:52:20 +0200

    al enviar formulario de búsqueda volver a dashboard para ver resultados

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,16 @@
 import React, { useRef } from 'react'
+import { useHistory } from 'react-router';
 
 function SearchBar(props) {
     const { submitHandler } = props;
     const termToSearch = useRef(null)
+    const history = useHistory()
 
     const initSearch = (e)=>{
         const currentTermToSearch = termToSearch.current.value;
         submitHandler(currentTermToSearch);
         e.preventDefault();
+        history.push('/')
     }
 
     return (
